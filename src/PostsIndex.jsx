aeae2df5e2ad198c3154a739f20af1278e513f9f
waykwo <72991933+waykwo@users.noreply.github.com>
2024-09-17 22:28:02 -0400

    Add more styling

--- src/PostsIndex.jsx
+++ src/PostsIndex.jsx
@@ -3,13 +3,15 @@ export function PostsIndex(props) {
   return (
     <div id="posts-index">
       <h1>All posts</h1>
-      {props.posts.map(post => (
-        <div key={post.id}>
-          <h2>{post.title}</h2>
-          <p>{post.body}</p>
-          <img src={post.image} />
-        </div>
-      ))}
+      <div className="cards">
+        {props.posts.map(post => (
+          <div key={post.id}>
+            <h2>{post.title}</h2>
+            <p>{post.body}</p>
+            <img src={post.image} />
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
