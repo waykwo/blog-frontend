996a1dd8c8f9201fb63c305d3c719d25d3ea7afd
waykwo <72991933+waykwo@users.noreply.github.com>
2024-09-18 21:15:45 -0400

    Install axios, create function to read api, button to get data

--- src/PostPage.jsx
+++ src/PostPage.jsx
@@ -1,36 +1,24 @@
 import { PostsIndex } from "./PostsIndex";
 import { PostsNew } from "./PostsNew";
+import axios from "axios";
+import { useState } from "react";
 
 export function PostPage() {
-  let name = "Test";
-  let posts = [
-    {
-      id: 1,
-      title: "Post 1",
-      body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet nobis fuga est tenetur commodi veritatis atque iusto laboriosam rerum iste quos, voluptas labore quae quibusdam alias possimus debitis non fugit.",
-      image: "https://th.bing.com/th/id/OIP.wwxK07x0Umfnh0l-nrjxjgHaDg?rs=1&pid=ImgDetMain",
-    },
-    {
-      id: 2,
-      title: "Post 2",
-      body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias debitis fugit, voluptate est sit quod ad repellat laborum id accusantium esse vero ea officia, optio commodi sunt qui placeat? Dolore!",
-      image: "https://www.sommertage.com/wp-content/uploads/2022/09/Wilder-Kaiser-Astbergsee-1140x700.jpg",
-    },
-    {
-      id: 3,
-      title: "Post 3",
-      body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias debitis fugit, voluptate est sit quod ad repellat laborum id accusantium esse vero ea officia, optio commodi sunt qui placeat? Dolore!",
-      image: "https://th.bing.com/th/id/OIP.PKfqOnjL_1h7NOJfE3OvkQHaE8?w=1920&h=1280&rs=1&pid=ImgDetMain",
-    },
-  ];
+  const [posts, setPosts] = useState([]);
+
+  const handleIndex = () => (
+    axios.get("http://localhost:3000/posts.json").then(response => {
+      console.log(response.data);
+      setPosts(response.data);
+    })
+  )
+  
 
   return (
     <main>
-
       <PostsNew />
-
       <PostsIndex name={name} posts={posts} />
-
+      <button onClick={handleIndex}>Get the data</button>
     </main>
 
   );
