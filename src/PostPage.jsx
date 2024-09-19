31c2e2289ec88a5af970639ff0c59d4629563b0c
waykwo <72991933+waykwo@users.noreply.github.com>
2024-09-18 21:59:43 -0400

    Fix PostPage

--- src/PostPage.jsx
+++ src/PostPage.jsx
@@ -6,12 +6,12 @@ import { useState, useEffect } from "react";
 export function PostPage() {
   const [posts, setPosts] = useState([]);
 
-  const handleIndex = () => (
+  const handleIndex = () => {
     axios.get("http://localhost:3000/posts.json").then(response => {
       console.log(response.data);
       setPosts(response.data);
-    })
-  )
+    });
+  };
   
   useEffect(handleIndex, []);
 
