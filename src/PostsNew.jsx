7ae6a2feac998108e1b2b7ca5cff53ee1a753676
waykwo <72991933+waykwo@users.noreply.github.com>
2024-09-22 12:46:21 -0400

    Add old school HTML POST action to PostsNew.jsx

--- src/PostsNew.jsx
+++ src/PostsNew.jsx
@@ -1,29 +1,51 @@
-export function PostsNew(props) {
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    const params = new FormData(event.target);
-    props.onCreatePost(params, () => event.target.reset());
-  }
-  return (
+export function PostsNew() {
+
+  // export function PostsNew(props) {
+//   const handleSubmit = (event) => {
+//     event.preventDefault();
+//     const params = new FormData(event.target);
+//     props.onCreatePost(params, () => event.target.reset());
+//   }
+
+return (
     <div id="posts-new">
       <h1>New post</h1>
-      <form onSubmit={handleSubmit}>
-        <ul>
-          <li>
+      
+      <form method="POST" action="http://localhost:3000/posts.json">
+        <div>
+          <div>
             <label>Title:</label>
             <input type="text" name="title" />
-          </li>
-          <li>
+          </div>
+          <div>
             <label>Body:</label>
             <textarea id="msg" name="body"></textarea>
-          </li>
-          <li>
+          </div>
+          <div>
             <label>Image:</label>
             <input type="img" name="image" />
-          </li>
-        </ul>
+          </div>
+        </div>
         <button type="submit">Submit</button>
       </form>
+
+      {/* <form onSubmit={handleSubmit}>
+        <div>
+          <div>
+            <label>Title:</label>
+            <input type="text" name="title" />
+          </div>
+          <div>
+            <label>Body:</label>
+            <textarea id="msg" name="body"></textarea>
+          </div>
+          <div>
+            <label>Image:</label>
+            <input type="img" name="image" />
+          </div>
+        </div>
+        <button type="submit">Submit</button>
+      </form> */}
     </div>
   );
 }
