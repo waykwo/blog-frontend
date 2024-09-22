1c48ed024567eb33df9c397071e35ab13eb72d12
waykwo <72991933+waykwo@users.noreply.github.com>
2024-09-22 13:15:26 -0400

    Add post request using Axios in PostsNew.jsx

--- src/PostsNew.jsx
+++ src/PostsNew.jsx
@@ -1,4 +1,18 @@
+import axios from 'axios';
+
 export function PostsNew() {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // get params from form
+    const params = new FormData(event.target);
+    // make web request to rails
+    axios.post("http://localhost:3000/posts.json", params).then(response => {
+      console.log(response.data);
+      event.target.reset();
+    });
+    // make sure to send params in web request
+    console.log('handling submit');
+  };
 
   // export function PostsNew(props) {
 //   const handleSubmit = (event) => {
@@ -11,25 +25,7 @@ return (
     <div id="posts-new">
       <h1>New post</h1>
       
-      <form method="POST" action="http://localhost:3000/posts.json">
-        <div>
-          <div>
-            <label>Title:</label>
-            <input type="text" name="title" />
-          </div>
-          <div>
-            <label>Body:</label>
-            <textarea id="msg" name="body"></textarea>
-          </div>
-          <div>
-            <label>Image:</label>
-            <input type="img" name="image" />
-          </div>
-        </div>
-        <button type="submit">Submit</button>
-      </form>
-
-      {/* <form onSubmit={handleSubmit}>
+      {/* <form method="POST" action="http://localhost:3000/posts.json">
         <div>
           <div>
             <label>Title:</label>
@@ -46,6 +42,24 @@ return (
         </div>
         <button type="submit">Submit</button>
       </form> */}
+
+      <form onSubmit={handleSubmit}>
+        <div>
+          <div>
+            <label>Title:</label>
+            <input type="text" name="title" />
+          </div>
+          <div>
+            <label>Body:</label>
+            <textarea id="msg" name="body"></textarea>
+          </div>
+          <div>
+            <label>Image:</label>
+            <input type="img" name="image" />
+          </div>
+        </div>
+        <button type="submit">Submit</button>
+      </form>
     </div>
   );
 }
